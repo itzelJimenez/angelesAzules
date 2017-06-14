8bb6d6cf0fae6995eec7aa97148e7a882730f8b5
Itzel <itzeljimenez.do@gmail.com>
2017-06-14 15:14:52 +0000

    login con facebook, se oculta formulario

--- js/driversingup.js
+++ js/driversingup.js
@@ -1,35 +1,25 @@
 (function(){
-
+var $logFacebook = $('#test1');
 var $checkAngel = $('#test5');
+var $btnFB = $('.fb-login-button');
 var cargarPagina = function(){
 	$('.modal').modal();
 	$checkAngel.click(mostrarBtnSubir);
+	$logFacebook.click(logFB);
+}
 
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-
-{
-    status: 'connected',
-    authResponse: {
-        accessToken: '...',
-        expiresIn:'...',
-        signedRequest:'...',
-        userID:'...'
-    }
+var logFB = function () {
+	console.log(this)
+	if($('#test1').checked = true){
+		$btnFB.removeClass('hide');
+		$('#form-registro').addClass('hide');
+		} else {$btnFB.addClass('hide')}
 }
 
 
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-}
 var validarCheck = function(){
 	if($('#test5').checked = true){
-		$('#contBtnSubir').removeClass('hide')
+		$('#contBtnSubir').removeClass('hide');
 		} else {$('#contBtnSubir').addClass('hide')}
 }
 var mostrarBtnSubir = function(){
