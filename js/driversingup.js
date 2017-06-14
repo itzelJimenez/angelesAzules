53c24cc59754dd759ebe789f42d1a4426d9c25b3
Itzel <itzeljimenez.do@gmail.com>
2017-06-14 15:27:18 +0000

    se oculta formulario, se muestra opcion de logear con fb

--- js/driversingup.js
+++ js/driversingup.js
@@ -1,13 +1,25 @@
 (function(){
-
+var $logFacebook = $('#test1');
 var $checkAngel = $('#test5');
+var $btnFB = $('.fb-login-button');
 var cargarPagina = function(){
 	$('.modal').modal();
 	$checkAngel.click(mostrarBtnSubir);
+	$logFacebook.click(logFB);
 }
+
+var logFB = function () {
+	console.log(this)
+	if($('#test1').checked = true){
+		$btnFB.removeClass('hide');
+		$('#form-registro').addClass('hide');
+		} else {$btnFB.addClass('hide')}
+}
+
+
 var validarCheck = function(){
 	if($('#test5').checked = true){
-		$('#contBtnSubir').removeClass('hide')
+		$('#contBtnSubir').removeClass('hide');
 		} else {$('#contBtnSubir').addClass('hide')}
 }
 var mostrarBtnSubir = function(){
